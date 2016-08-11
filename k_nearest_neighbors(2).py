0f3cce5973e1e96fe7755d287786e4ff596e09aa
profmouse <tappiera00@gmail.com>
2016-08-11 21:45:47 +0200

    finished custom k-nearest neighbors

--- k_nearest_neighbors(2).py
+++ k_nearest_neighbors(2).py
@@ -1,9 +1,33 @@
 from math import sqrt
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+import warnings
+from collections import Counter
+style.use('fivethirtyeight')
 
-plot1 = [1,3]
-plot2 = [2,5]
+dataset = {'m': [[1,2],[3,4],[4,3]], 'g': [[7,8],[8,10],[9,9]]}
+predict = [1,4]
 
-def calculate_distance(point1, point2):
-    return sqrt((plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2)
+def k_nearest_neighbors(data, predict, k=3):
+    distances = []
+    for cluster in dataset:
+        for feature in dataset[cluster]:
+            euclidean_distance = np.linalg.norm(np.array(feature) - np.array(predict))
+            distances.append([euclidean_distance, cluster])
 
-print(calculate_distance(plot1, plot2))
+    # for i in sorted(distances):
+    #     print(i)
+    #     print(i[1])
+
+    votes = [i[1] for i in sorted(distances)[:k]]
+    return Counter(votes).most_common(1)
+
+
+result = k_nearest_neighbors(dataset,predict)
+print(result)
+
+[[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]] for i in dataset]
+plt.scatter(predict[0],predict[1],color=result[0][0])
+
+plt.show()
